c70dd3b485c1e5a15016f065def5e0d26861c35e
Thomas Voets <voets_thomas@hotmail.com>
2020-12-23 16:09:19 +0100

    Practicing Directives - Solution Code

--- 02-The-Basics/practicing-directives/src/app/app.component.ts
+++ 02-The-Basics/practicing-directives/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showSecret: boolean = false;
+  numberOfClick: number = 0;
+
+  logItems: number[] = [];
+
+  onToggleDetails(): void {
+    this.showSecret = !this.showSecret;
+
+    this.numberOfClick++;
+    this.logItems.push(this.numberOfClick)
+  }
 
 }
