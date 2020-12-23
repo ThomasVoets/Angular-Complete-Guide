e46ae64021214f229c3b94c60751bd6bb6936cd3
Thomas Voets <voets_thomas@hotmail.com>
2020-12-23 14:38:15 +0100

    Practicing Databinding - Solution Code

--- 02-The-Basics/practicing-databinding/src/app/app.component.ts
+++ 02-The-Basics/practicing-databinding/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  username: string = '';
+
+  onResetUsername(): void {
+    this.username = '';
+  }
+
 }
